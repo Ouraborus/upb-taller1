6a3aa55758688e9f664f2cb0c6f1b23b1e4b5b3a
Ouraborus <1222eca@gmail.com>
2018-01-29 20:53:36 -0500

    Adding Gallery to the project, still working with Movies Grid

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const HtmlWebpackPlugin = require('html-webpack-plugin') // installed via npm
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack'); //to access built-in plugins    
-const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
 
 const config = {
-  entry: ['./src/js/gallery/index.js','./src/js/movies/indexMovies.js'],
+  entry: ['./src/js/gallery/index.js', './src/js/movies/indexMovies.js', './src/sass/main.scss'],
   devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
-    rules:  [
-      /*
-      your other rules for JavaScript transpiling go in here
-      */
+    rules: [
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          use: [
-            'css-loader',
-            'sass-loader?sourceMap'
-          ]
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader?sourceMap']
         })
       }
-
     ]
   },
-  plugins:[    
-    new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({template: './src/index.html'})
-]
-};
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        beautify: false,
+        ecma: 6,
+        compress: true,
+        comments: false
+      }
+    }),
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new HtmlWebpackPlugin({filename: 'gallery.html', template: './src/gallery.html'}),
+    new HtmlWebpackPlugin({filename: 'movies.html', template: './src/movies.html'})
+  ]
+}
 
-module.exports = config;
+module.exports = config
